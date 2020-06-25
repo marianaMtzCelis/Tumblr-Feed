e32f43e91a7a1a28d4924c2abaaebea155e76efe
Mariana Martinez <marianamtzcelis@fb.com>
2020-06-25 12:14:16 -0500

    Built the photo feed

---
+++ Tumblr Feed/PhotosTableViewCell.h
@@ -0,0 +1,20 @@
+//
+//  PhotosTableViewCell.h
+//  Tumblr Feed
+//
+//  Created by Mariana Martinez on 25/06/20.
+//  Copyright © 2020 Mariana Martinez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotosTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *photoCellView;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Tumblr Feed/PhotosViewController.m
+++ Tumblr Feed/PhotosViewController.m
@@ -7,10 +7,14 @@
 //
 
 #import "PhotosViewController.h"
+#import "PhotosTableViewCell.h"
+#import "UIImageView+AFNetworking.h"
 
-@interface PhotosViewController ()
+@interface PhotosViewController () <UITableViewDataSource, UITableViewDelegate>
 
 @property (strong, nonatomic) NSArray *posts;
+
+@property (weak, nonatomic) IBOutlet UITableView *photosTableView;
 
 @end
 
@@ -18,6 +22,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.photosTableView.dataSource = self;
+    self.photosTableView.delegate = self;
     
     NSURL *url = [NSURL URLWithString:@"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
@@ -41,9 +48,45 @@
                 //NSLog(@"%@", self.posts);
                 
                 // TODO: Reload the table view
+                [self.photosTableView reloadData];
             }
         }];
     [task resume];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.posts.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    PhotosTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PhotosTableViewCell"];
+    
+    // forIndexPath:indexPath
+       
+    NSDictionary *post = self.posts[indexPath.row];
+    
+    NSArray *photos = post[@"photos"];
+    if (photos) {
+        
+       // 1. Get the first photo in the photos array
+       NSDictionary *photo = photos[0];
+
+       // 2. Get the original size dictionary from the photo
+       NSDictionary *originalSize =  photo[@"original_size"];
+
+       // 3. Get the url string from the original size dictionary
+       NSString *urlString = originalSize[@"url"];
+
+       // 4. Create a URL using the urlString
+       NSURL *url = [NSURL URLWithString:urlString];
+        
+        [cell.photoCellView setImageWithURL:url];
+        
+        self.photosTableView.rowHeight = 240;
+    }
+    
+    return cell;
 }
 
 /*
